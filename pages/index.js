af43630dfe115fb597e556ef5411ce0b824328f2
IJLAL <ijlal.tanveer294@gmail.com>
2024-11-25 22:16:46 +0500

    updated image

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
             <meta property="og:title" content="My Static Site" />
             <meta property="og:description" content="This is a sample static site using Next.js." />
         </Head>
-        <h1>Welcome to My Static Site</h1>
-        <img src="./cat.jpg" alt="Example Image" loading="lazy" />
+        <h1>A Sample My Static Site</h1>
+        <img src="https://i.pinimg.com/236x/73/8b/59/738b59f109c56190cbe22d4c148a06f2.jpg" alt="Example Image" loading="lazy" />
     </>
  );
 }
